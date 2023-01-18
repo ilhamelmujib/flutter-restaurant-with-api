5666358998294e320ca09822802fa8bba29b542a
ilhamelmujib <ilham.elmujib@gmail.com>
2023-01-18 15:58:43 +0700

    enhancement detail page

--- lib/ui/detail_page.dart
+++ lib/ui/detail_page.dart
@@ -14,7 +14,8 @@ class DetailPage extends StatelessWidget {
       body: Consumer<RestaurantProvider>(
         builder: (context, state, _) {
           if (state.state == ResultState.loading) {
-            return const Center(child: CircularProgressIndicator(color: Colors.black));
+            return const Center(
+                child: CircularProgressIndicator(color: Colors.black));
           } else if (state.state == ResultState.hasData) {
             return _buildDetail(context, state.resultDetail.restaurant!);
           } else if (state.state == ResultState.noData) {
@@ -90,37 +91,83 @@ Widget _buildDetail(BuildContext context, Restaurant restaurant) {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Icon(
-                      Icons.location_on,
-                      color: Colors.grey,
-                      size: 15,
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Material(
+                          color: Colors.white,
+                          elevation: 3,
+                          borderRadius: BorderRadius.circular(10),
+                          clipBehavior: Clip.antiAlias,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 7, vertical: 2),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const Icon(
+                                  Icons.star,
+                                  color: Colors.orange,
+                                  size: 15,
+                                ),
+                                const SizedBox(
+                                  width: 3,
+                                ),
+                                Text(restaurant.rating.toString())
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                     const SizedBox(
-                      width: 5,
+                      width: 10,
                     ),
-                    Flexible(
-                      child: Text(
-                        restaurant.city!,
-                        maxLines: 1,
-                        style: const TextStyle(color: Colors.grey),
-                        overflow: TextOverflow.ellipsis,
-                      ),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.location_on,
+                          color: Colors.grey,
+                          size: 15,
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          restaurant.city!,
+                          maxLines: 1,
+                          style: const TextStyle(color: Colors.grey),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
                     ),
                   ],
                 ),
                 const SizedBox(
                   height: 10,
                 ),
-                Text(
-                  "Rating ${restaurant.rating!}",
-                  maxLines: 1,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
                 Text(restaurant.description!),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: restaurant.categories!.map((category) {
+                    return Container(
+                      margin: const EdgeInsets.all(2.0),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 5),
+                      decoration: BoxDecoration(
+                          color: Colors.orangeAccent,
+                          border: Border.all(color: Colors.orangeAccent)),
+                      child: Text(
+                        category!.name!,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    );
+                  }).toList(),
+                ),
                 const SizedBox(
                   height: 20,
                 ),
@@ -138,7 +185,7 @@ Widget _buildDetail(BuildContext context, Restaurant restaurant) {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: restaurant.menus!.foods!.map((menu) {
-                    return _buildItem(context, menu!);
+                    return _buildMenu(context, menu!);
                   }).toList(),
                 ),
                 const SizedBox(
@@ -158,9 +205,24 @@ Widget _buildDetail(BuildContext context, Restaurant restaurant) {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: restaurant.menus!.drinks!.map((menu) {
-                    return _buildItem(context, menu!);
+                    return _buildMenu(context, menu!);
                   }).toList(),
-                )
+                ),
+                const SizedBox(
+                  height: 19,
+                ),
+                const Text(
+                  "Reviews",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                Column(
+                    children: restaurant.customerReviews!.map((review) {
+                  return _buildReview(context, review!);
+                }).toList())
               ],
             ),
           ),
@@ -170,11 +232,40 @@ Widget _buildDetail(BuildContext context, Restaurant restaurant) {
   );
 }
 
-Widget _buildItem(BuildContext context, Category category) {
+Widget _buildMenu(BuildContext context, Category category) {
   return Text(
     "- ${category.name!}",
     style: const TextStyle(
       fontSize: 16,
+    ),
+  );
+}
+
+Widget _buildReview(BuildContext context, CustomerReview review) {
+  return Container(
+    width: double.infinity,
+    padding: const EdgeInsets.all(15),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          review.name!,
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+        const SizedBox(
+          height: 2,
+        ),
+        Text(
+          review.review!,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          review.date!,
+          style: const TextStyle(color: Colors.grey),
+        ),
+      ],
     ),
   );
 }
